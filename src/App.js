187913dde7aa4604a7b5c0cbe61eab18196586d9
Sultanbek <sultan.02173@gmail.com>
2023-12-03 16:51:18 +0600

    dz3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash)
+  console.log("state: ", cash);
+  const addCash = () => {
+    dispatch({ type: 'ADD_CASH', payload: 1 })
+  }
+
+  const getCash = () => {
+    dispatch({ type: 'GET_CASH', payload: 1  })
+  }
+
+  const getFCash = () =>{
+    dispatch({ type: 'ADD_CASH', payload: 5 })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>state: {cash}</h1>
+      <button onClick={() => addCash()} style={{ border: "1px solid", padding: '1rem' }}>ADD_CASH</button>
+      <button onClick={() => getCash()} style={{ border: "1px solid", padding: '1rem' }}>GET_CASH</button>
+      <button onClick={() => getFCash()} style={{ border: "1px solid", padding: '1rem' }}>ADD_CASH 5</button>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const defaultValue = {
+  cash: 10
+}
+
+
+const reduser = (state = defaultValue, action) => {
+  switch (action.type) {
+
+    case "ADD_CASH":
+      return { ...state, cash: state.cash + action.payload }
+
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload }
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(reduser)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
